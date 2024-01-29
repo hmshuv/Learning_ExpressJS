4f4e5d51e4b8c787ec947f58a01b7a602649e684
Himanshu Gupta <himanshugupta543210@gmail.com>
2024-01-29 12:33:25 +0530

    commit

--- index.js
+++ index.js
@@ -7,4 +7,8 @@ app.get('/contact',(req,res) => res.send('This is about page!'));
 app.get('/books/:bookId', (req, res) => {
     res.send(req.params);
   });
+// For invalid routes
+app.get('*', (req, res) => {
+    res.send('404! This is an invalid URL.');
+  });
 app.listen(6900, () => console.log('Example app listening at port 6900!'));
