c35c1e567b33b8b15fb998781410574d725fc27a
Himanshu Gupta <himanshugupta543210@gmail.com>
2024-01-29 12:23:48 +0530

    commit

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res)=>res.send('Hello World!!'));
+app.get('/about', (req,res) => res.send('This is about page!'));
+app.get('/contact',(req,res) => res.send('This is about page!'));
 app.listen(6900, () => console.log('Example app listening at port 6900!'));
